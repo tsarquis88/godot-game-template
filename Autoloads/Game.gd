0973c7b679285c2d66c8b1cb8684cb1ed6f6ddfa
Tomas Sarquis <tomas.sarquis@wazuh.com>
2022-09-30 09:31:53 -0300

    Improves pointed by @agustinoli

--- Autoloads/Game.gd
+++ Autoloads/Game.gd
@@ -5,21 +5,13 @@ signal ChangeScene
 signal Exit
 
 
-var nextScene
-
-
 func _ready()->void:
 	connect("Exit", self.on_Exit)
 	connect("ChangeScene", self.on_ChangeScene)
 
 
-func on_ChangeScene(scene)->void:
-	nextScene = scene 
-	switch_scene()
-
-
-func switch_scene()->void:
-	get_tree().change_scene_to_file(nextScene)
+func on_ChangeScene(newScene)->void:
+	get_tree().change_scene_to_file(newScene)
 
 
 func on_Exit()->void:
