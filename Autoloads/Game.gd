00f016a04e0c4792414ec7e7a33e2aaab3f29147
Tomas Sarquis <tomas.sarquis@wazuh.com>
2022-10-01 16:28:10 -0300

    Basic fadeScreen implemented

--- Autoloads/Game.gd
+++ Autoloads/Game.gd
@@ -5,13 +5,30 @@ signal ChangeScene
 signal Exit
 
 
+@export var firstScene = "Demo.tscn"
+@onready var fadeScreen = find_child("FadeScreen")
+@onready var nextScene = firstScene
+@onready var currentSceneNode = null
+
+
 func _ready()->void:
 	connect("Exit", self.on_Exit)
 	connect("ChangeScene", self.on_ChangeScene)
+	fadeScreen.connect("Waiting", self.instantiateCurrentScene)
+	instantiateCurrentScene()
+
+
+func instantiateCurrentScene():
+	if currentSceneNode != null:
+		currentSceneNode.queue_free()
+	
+	currentSceneNode = load(nextScene).instantiate() 
+	add_child(currentSceneNode)
 
 
 func on_ChangeScene(newScene)->void:
-	get_tree().change_scene_to_file(newScene)
+	fadeScreen.init()
+	nextScene = newScene
 
 
 func on_Exit()->void:
