264d1bbda4269270aac0bfb14e381ce4d08b96c0
Tomas Sarquis <tomas.sarquis@wazuh.com>
2022-10-04 22:09:20 -0300

    Reabase and finish PR?

--- MainMenu/MainMenu.gd
+++ MainMenu/MainMenu.gd
@@ -5,16 +5,17 @@ extends BoxContainer
 @onready var optionsButton = find_child("OptionsButton")
 @onready var creditsButton = find_child("CreditsButton")
 @onready var exitButton = find_child("ExitButton")
+@onready var madeWithLabel = find_child("MadeWithLabel")
 @onready var Game = get_parent()
 
 
 func _ready():
-	# TODO: add retranslate mechanics
-	
 	newGameButton.connect("pressed", self.on_newGameButton_pressed)
 	optionsButton.connect("pressed", self.on_optionsButton_pressed)
 	creditsButton.connect("pressed", self.on_creditsButton_pressed)
 	exitButton.connect("pressed", self.on_exitButton_pressed)
+	Language.connect("ReTranslate", self.reTranslate)
+	reTranslate()
 
 
 func on_newGameButton_pressed():
@@ -33,5 +34,12 @@ func on_creditsButton_pressed():
 
 
 func on_exitButton_pressed():
-	# TODO
-	print("Exit")
+	Game.emit_signal("Exit")
+
+
+func reTranslate():
+	newGameButton.text = tr("NEW-GAME")
+	optionsButton.text = tr("OPTIONS")
+	creditsButton.text = tr("CREDITS")
+	madeWithLabel.text = str(tr("MADE-WITH"), " Godot ", Engine.get_version_info().string)
+	exitButton.text = tr("EXIT")
