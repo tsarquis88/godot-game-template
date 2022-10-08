218c1cd46a445e300c476ee1919ea1ff19d2ecdd
Tomas Sarquis <tomas.sarquis@wazuh.com>
2022-10-07 22:41:26 -0300

    Language setting persistance added

--- Autoloads/Language.gd
+++ Autoloads/Language.gd
@@ -4,12 +4,22 @@ extends Node
 signal ReTranslate
 
 
-# TODO: Add language persistance after exiting
+const CONFIG_FILE_PATH = "res://config.cfg"
+
+
+var config = ConfigFile.new()
+
+
+func _ready():
+	if config.load(CONFIG_FILE_PATH) == OK:
+		set_language(config.get_value("language", "LANGUAGE"))
 
 
 func set_language(value : String) -> void:
 	TranslationServer.set_locale(value)
 	emit_signal("ReTranslate")
+	config.set_value("language", "LANGUAGE", value)
+	config.save(CONFIG_FILE_PATH)
 
 
 func get_language() -> String:
