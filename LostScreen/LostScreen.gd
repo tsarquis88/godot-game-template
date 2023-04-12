e29aee08e89a027efab083ecc7d99fbec51e3fe4
Tomas Sarquis <tsarquis88@gmail.com>
2023-04-12 20:42:25 +0200

    Fix typos from last commit

--- LostScreen/LostScreen.gd
+++ LostScreen/LostScreen.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @export var nextScene = "res://MainMenu/MainMenu.tscn"
 @export var duration = 3
-@onready var lostLabel = find_child("WonLabel")
+@onready var lostLabel = find_child("LostLabel")
 @onready var background = find_child("Background")
 @onready var Game = get_parent()
 
@@ -10,10 +10,10 @@ var timer
 
 func _ready():
 	background.color = Settings.MENU_BACKGROUND_COLOR
-	lostLabel.text = tr("WON-MESSAGE")
+	lostLabel.text = tr("LOST-MESSAGE")
 	timer = GlobalTimer.create_timeout(self.finish, duration, true, true)
 	
-	Logger.logDebug("WonScreen: Ready")
+	Logger.logDebug("LostScreen: Ready")
 
 
 func finish():
--- WonScreen/WonScreen.gd
+++ WonScreen/WonScreen.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @export var nextScene = "res://MainMenu/MainMenu.tscn"
 @export var duration = 3
-@onready var lostLabel = find_child("LostLabel")
+@onready var wonLabel = find_child("WonLabel")
 @onready var background = find_child("Background")
 @onready var Game = get_parent()
 
@@ -10,10 +10,10 @@ var timer
 
 func _ready():
 	background.color = Settings.MENU_BACKGROUND_COLOR
-	lostLabel.text = tr("LOST-MESSAGE")
+	wonLabel.text = tr("WON-MESSAGE")
 	timer = GlobalTimer.create_timeout(self.finish, duration, true, true)
 	
-	Logger.logDebug("LostScreen: Ready")
+	Logger.logDebug("WonScreen: Ready")
 
 
 func finish():
