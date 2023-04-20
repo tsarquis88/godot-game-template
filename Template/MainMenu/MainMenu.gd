1deaaa2a6b8ef0bc1773da0fb5092599012a8dbe
Tomas Sarquis <42682247+tsarquis88@users.noreply.github.com>
2023-04-20 19:20:23 +0200

    Add score table scene (#42)
    * Add score table scene
    
    * Add no-scores label

--- Template/MainMenu/MainMenu.gd
+++ Template/MainMenu/MainMenu.gd
@@ -3,11 +3,13 @@ extends BoxContainer
 const PLAYGROUND_SCENE = "res://Template/Playground/Playground.tscn"
 const CREDITS_SCENE = "res://Template/Credits/Credits.tscn"
 const OPTIONS_SCENE = "res://Template/OptionsMenu/OptionsMenu.tscn"
+const SCORE_TABLE_SCENE = "res://Template/ScoreTable/ScoreTable.tscn"
 const BUTTON_SOUND = "button.wav"
 
 @onready var m_background = find_child("Background")
 @onready var m_new_game_button = find_child("NewGameButton")
 @onready var m_options_button = find_child("OptionsButton")
+@onready var m_score_table_button = find_child("ScoreTableButton")
 @onready var m_credits_button = find_child("CreditsButton")
 @onready var m_exit_button = find_child("ExitButton")
 @onready var m_made_with_label = find_child("MadeWithLabel")
@@ -18,10 +20,12 @@ func _ready():
 	m_background.color = Settings.MENU_BACKGROUND_COLOR
 	m_new_game_button.connect("pressed", self.on_new_game_button_pressed)
 	m_options_button.connect("pressed", self.on_options_button_pressed)
+	m_score_table_button.connect("pressed", self.on_score_table_button_pressed)
 	m_credits_button.connect("pressed", self.on_credits_button_pressed)
 	m_exit_button.connect("pressed", self.on_exit_button_pressed)
 	m_new_game_button.connect("pressed", self.on_button_pressed)
 	m_options_button.connect("pressed", self.on_button_pressed)
+	m_score_table_button.connect("pressed", self.on_button_pressed)
 	m_credits_button.connect("pressed", self.on_button_pressed)
 	m_exit_button.connect("pressed", self.on_button_pressed)
 	Language.connect("re_translate", self.re_translate)
@@ -42,6 +46,10 @@ func on_credits_button_pressed():
 	m_game.emit_signal("change_scene", CREDITS_SCENE, GameSettings.TRANSITIONS.UP_BOTTOM)
 
 
+func on_score_table_button_pressed():
+	m_game.emit_signal("change_scene", SCORE_TABLE_SCENE, GameSettings.TRANSITIONS.UP_BOTTOM)
+
+
 func on_exit_button_pressed():
 	m_game.emit_signal("exit")
 
@@ -53,6 +61,7 @@ func on_button_pressed():
 func re_translate():
 	m_new_game_button.text = tr("NEW-GAME")
 	m_options_button.text = tr("OPTIONS")
+	m_score_table_button.text = tr("SCORE-TABLE")
 	m_credits_button.text = tr("CREDITS")
 	m_made_with_label.text = str(tr("MADE-WITH"), " Godot ", Engine.get_version_info().string)
 	m_exit_button.text = tr("EXIT")
---
+++ Template/ScoreTable/TableRow.gd
@@ -0,0 +1,9 @@
+extends HBoxContainer
+
+@onready var m_player_name_label = find_child("PlayerNameLabel")
+@onready var m_score_label = find_child("ScoreLabel")
+
+
+func set_row(player_name: String, score: String):
+	m_player_name_label.text = player_name
+	m_score_label.text = score
