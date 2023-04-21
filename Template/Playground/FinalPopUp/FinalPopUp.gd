06378db2ba0944b3e9a4b4c6bf6edc26c1f93ddd
Tomas Sarquis <42682247+tsarquis88@users.noreply.github.com>
2023-04-21 18:48:25 +0200

    Add score recording (#47)
    * Add score recording
    
    * Remove deprecated scenes and fix score
    
    * Style fixes

---
+++ Template/Playground/FinalPopUp/FinalPopUp.gd
@@ -0,0 +1,52 @@
+extends Panel
+
+signal accept
+
+const BUTTON_SOUND = "button.wav"
+const SCORES_FILENAME = "res://scores.txt"
+
+@onready var m_title = find_child("Title")
+@onready var m_accept_button = find_child("AcceptButton")
+@onready var m_text_edit = find_child("TextEdit")
+@onready var m_score = 0
+
+
+func _ready():
+	var style_box = get_theme_stylebox("panel")
+	style_box.bg_color = Settings.MENU_BACKGROUND_COLOR
+	style_box.border_color = Settings.MENU_BORDER_COLOR
+
+	m_accept_button.connect("pressed", self.on_accept_button_pressed)
+	m_accept_button.connect("pressed", self.on_button_pressed)
+
+	m_accept_button.text = tr("ACCEPT")
+	m_text_edit.placeholder_text = tr("ENTER-PLAYER-NAME")
+
+	Logger.log_debug("FinalPopUp: Ready")
+
+
+func set_up(won: bool, score: int):
+	if won:
+		m_title.text = tr("WON-MESSAGE")
+	else:
+		m_title.text = tr("LOST-MESSAGE")
+
+	m_text_edit.visible = score > 0
+	m_score = score
+
+
+func on_accept_button_pressed():
+	var player_name = m_text_edit.text
+	if m_text_edit.visible and not player_name.is_empty():
+		# Record score into filesystem.
+		Logger.log_debug(str("Recording score '", m_score, "' for player '", player_name, "'"))
+		var file = FileAccess.open(SCORES_FILENAME, FileAccess.READ_WRITE)
+		file.seek_end()
+		file.store_csv_line([m_text_edit.text, m_score, Time.get_date_string_from_system()])
+		file.close()
+
+	emit_signal("accept")
+
+
+func on_button_pressed():
+	SfxManager.play_sfx(BUTTON_SOUND)
--- Template/Playground/Playground.gd
+++ Template/Playground/Playground.gd
@@ -3,12 +3,14 @@ extends Node2D
 const WIN_SOUND = "win.wav"
 const GAME_MUSIC = "playable.ogg"
 const INIT_MUSIC = "init.ogg"
+const NEXT_SCENE = "res://Template/MainMenu/MainMenu.tscn"
 
 @onready var m_pause = false
 @onready var m_pause_menu = find_child("PauseMenu")
 @onready var m_pause_menu_options = find_child("PauseMenuOptions")
 @onready var m_playable = find_child("Playable")
 @onready var m_hud = find_child("HUD")
+@onready var m_final_pop_up = find_child("FinalPopUp")
 @onready var m_game = get_parent()
 
 
@@ -29,27 +31,22 @@ func _ready():
 	m_pause_menu.connect("resume", self.on_pause_menu_resume)
 	m_pause_menu_options.connect("back", self.on_pause_menu_options_return)
 	m_playable.connect("end", self.on_playable_end)
+	m_final_pop_up.visible = false
+	m_final_pop_up.connect("accept", self.on_final_pop_up_accept)
 	SfxManager.play_music(GAME_MUSIC)
 
 	Logger.log_debug("Playground: Ready")
 
 
-# Handles the 'end' signal from Playable, changing the scene to the WonScreen
-# or LostScreen scene depending of the game results.
-func on_playable_end(won: bool):
-	Logger.log_debug(str("Playground: Playable ends with won = ", won))
+# Handles the 'end' signal from Playable, showing the PopUp depending on how the game ended.
+func on_playable_end(won: bool, score: int):
+	Logger.log_debug(str("Playground: Playable ends with won = ", won, " and score = ", score))
 
-	var next_scene
+	m_playable.set_pause(true)
+	m_hud.set_pause(true)
 
-	if won:
-		SfxManager.play_sfx(WIN_SOUND)
-		next_scene = "res://Template/WonScreen/WonScreen.tscn"
-	else:
-		# SfxManager.play_sfx(LOST_SOUND)
-		next_scene = "res://Template/LostScreen/LostScreen.tscn"
-
-	SfxManager.play_music(INIT_MUSIC)
-	m_game.emit_signal("change_scene", next_scene, GameSettings.TRANSITIONS.FADE_SCREEN)
+	m_final_pop_up.set_up(won, score)
+	m_final_pop_up.visible = true
 
 
 # Handles the 'exit' button from the pause menu, returning to the MainMenu scene.
@@ -63,8 +60,8 @@ func on_pause_menu_exit_game():
 	)
 
 
-# Handles the 'resume' button from the pause menu, resuming the game by calling
-# the set_pause() method of Playable and HUD scenes.
+# Handles the 'resume' button from the pause menu, resuming the game by calling the set_pause()
+# method of Playable and HUD scenes.
 func on_pause_menu_resume():
 	m_playable.set_pause(false)
 	m_hud.set_pause(false)
@@ -77,8 +74,13 @@ func on_pause_menu_options():
 	m_pause_menu_options.visible = true
 
 
-# Handles the 'return' button from the options menu, returning to the
-# pause menu.
+# Handles the 'return' button from the options menu, returning to the pause menu.
 func on_pause_menu_options_return():
 	m_pause_menu.visible = true
 	m_pause_menu_options.visible = false
+
+
+# Handels the 'accept' button from the pop up, returning to the main menu scene.
+func on_final_pop_up_accept():
+	SfxManager.play_music(INIT_MUSIC)
+	m_game.emit_signal("change_scene", NEXT_SCENE, GameSettings.TRANSITIONS.FADE_SCREEN)
