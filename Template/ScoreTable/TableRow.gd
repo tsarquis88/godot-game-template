2556cba0e12d4bd0b87ed0b211afe60f4cf7a0ec
Tomas Sarquis <tsarquis88@gmail.com>
2023-04-21 18:55:03 +0200

    Add date to score table

--- Template/ScoreTable/TableRow.gd
+++ Template/ScoreTable/TableRow.gd
@@ -2,8 +2,10 @@ extends HBoxContainer
 
 @onready var m_player_name_label = find_child("PlayerNameLabel")
 @onready var m_score_label = find_child("ScoreLabel")
+@onready var m_date_label = find_child("DateLabel")
 
 
-func set_row(player_name: String, score: String):
+func set_row(player_name: String, score: String, date: String):
 	m_player_name_label.text = player_name
 	m_score_label.text = score
+	m_date_label.text = date
